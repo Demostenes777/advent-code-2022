9bfd423991ab02bc298a2fa72774c69f70fafcac
Alaitz Mendiola <amendiol@redhat.com>
2022-12-02 18:25:12 +0100

    day 2 puzzle 2

---
+++ day-2/puzzle_2.py
@@ -0,0 +1,39 @@
+input = open("/home/alaitz/Development/advent-code-2022/day-2/input.txt", "r")
+
+lines = input.readlines()
+
+points = 0
+
+x = 1
+y = 2
+z = 3
+
+lost = 0
+draw = 3
+win = 6
+
+for line in lines:
+    # A,X = Rock 
+    # B,Y = Paper 
+    # C,Z = Scissors 
+    line = str.replace(line, "\n", "")
+    if line == 'A X':
+        points = points + lost + z
+    elif line == 'A Y':
+        points = points + draw + x
+    elif line == 'A Z':
+        points = points + win + y
+    elif line == 'B X':
+        points = points + lost + x
+    elif line == 'B Y':
+        points = points + draw + y
+    elif line == 'B Z':
+        points = points + win + z
+    elif line == 'C X':
+        points = points + lost + y
+    elif line == 'C Y':
+        points = points + draw + z
+    elif line == 'C Z':
+        points = points + win + x
+
+print(points)
