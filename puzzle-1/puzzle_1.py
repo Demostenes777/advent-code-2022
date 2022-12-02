a97c23a80ccb5687cc17dc9b2bb4cb5a51122525
Alaitz Mendiola <amendiol@redhat.com>
2022-12-02 17:29:05 +0100

    Finished with puzzle 1

---
+++ puzzle-1/puzzle_1.py
@@ -0,0 +1,14 @@
+input = open("/home/alaitz/Development/advent-code-2022/puzzle-1/input.txt", "r")
+
+lines = input.readlines()
+max = 0
+sum = 0
+
+for line in lines:
+    if line == '\n':
+        if sum > max:
+            max = sum
+        sum = 0
+    else:
+        sum = sum + int(str.replace(line, "\n", ""))
+print(max)
